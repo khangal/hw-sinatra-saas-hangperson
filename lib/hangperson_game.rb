7216ba36e5ec69be3bbf7b181df9c139de9cd206
Khangal <khangalkola@gmail.com>
2016-05-16 15:04:18 +0000

    part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,14 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +24,34 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def guess(char)
+    
+    raise(ArgumentError) if char.nil? || char =~ /[^a-z]/i || char.empty? 
+    char.downcase!
+    return false if @wrong_guesses.include?(char) || @guesses.include?(char)
+    
+    if @word.include? char
+      @guesses << char
+    else
+      @wrong_guesses << char
+    end
+    
+    return true
+  end
+  
+  def word_with_guesses
+    if guesses.empty? 
+      @word.gsub(/[a-z]/i, '-') 
+    else
+      @word.gsub(/[^#{@guesses}]/i, '-') 
+    end
+  end
+  
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    return :win if @word.chars.all? {|c| @guesses.include?(c) }
+    return :play
+  end
+  
 end
